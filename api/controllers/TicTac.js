5cb2f5c429a150617f4c400e6250678df3f23a95
mugenga <rockyyves@gmail.com>
2021-07-04 12:56:37 +0200

    Completed Task

--- api/controllers/TicTac.js
+++ api/controllers/TicTac.js
@@ -1,4 +1,21 @@
 module.exports = class TicTac {
+  get_prospective_board(board, player) {
+    // Go through all possible moves the player is expected to make
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] == " ") {
+        return this.move(board, i, player);
+      }
+    }
+  }
+
+  move(board, index, player) {
+    let splitted_board = board.split("");
+    splitted_board[index] = player;
+    console.log(splitted_board);
+    console.log(splitted_board.join(""));
+    return splitted_board.join("");
+  }
+
   // Method to check whether board is valid
   is_valid(board) {
     // Calculate whether its o's turn
@@ -29,40 +46,108 @@ module.exports = class TicTac {
 
   // Method to check which player is the winner
   is_player_winner(board, player) {
-    console.log("hey");
     const win = [player, player, player];
+
+    // Check win variable against all possible winning scenarios
     const win_horizontal = [
-      [board[0], board[1], board[2]] === win,
-      [board[3], board[4], board[5]] === win,
-      [board[6], board[7], board[8]] === win,
+      this.arrayMatch([board[0], board[1], board[2]], win),
+      this.arrayMatch([board[3], board[4], board[5]], win),
+      this.arrayMatch([board[6], board[7], board[8]], win),
     ];
     const win_vertical = [
-      [board[0], board[3], board[6]] === win,
-      [board[1], board[4], board[7]] === win,
-      [board[2], board[5], board[8]] === win,
+      this.arrayMatch([board[0], board[3], board[6]], win),
+      this.arrayMatch([board[1], board[4], board[7]], win),
+      this.arrayMatch([board[2], board[5], board[8]], win),
     ];
     const win_diagonal = [
-      [board[0], board[4], board[8]] === win,
-      [board[2], board[4], board[6]] === win,
+      this.arrayMatch([board[0], board[4], board[8]], win),
+      this.arrayMatch([board[2], board[4], board[6]], win),
     ];
 
-    console.log(win_vertical);
-    console.log(win_horizontal);
-    console.log(win_diagonal);
-    console.log(board[0]);
-    console.log(board[1]);
-    console.log(board[2]);
-    console.log(board[3]);
-    console.log(board[4]);
-    console.log(board[5]);
-
-    return this.any(win_horizontal) || this.any(win_vertical) || this.any(win_diagonal);
+    return (
+      this.any(win_horizontal) ||
+      this.any(win_vertical) ||
+      this.any(win_diagonal)
+    );
   }
 
+  // Method to check if two array match
+  arrayMatch(line, win) {
+    return JSON.stringify(line) == JSON.stringify(win);
+  }
+
+  // Method to find if there any true value in an array
   any(iterable) {
     for (var index = 0; index < iterable.length; index++) {
       if (iterable[index]) return true;
     }
     return false;
+  }
+
+  get_move(board, player) {
+    //Given a board and the computer's letter, determine where to move and return that move.
+    const human = "o";
+    const computer = "x";
+
+    // Game Algorithm
+
+    // Check if computer can win in the next move
+    for (let i = 0; i <= 8; i++) {
+      // Get a copy of the array
+      let copy = board.slice();
+      if (!this.is_game_tie(copy)) {
+        this.makeMove(copy, computer, i);
+        if (this.is_player_winner(copy, computer)) {
+          return i;
+        }
+      }
+    }
+
+    //Check if human can win on next move, and block them.
+    for (let i = 0; i <= 8; i++) {
+      let copy = board.slice();
+      if (!this.is_game_tie(copy)) {
+        this.makeMove(copy, human, i);
+        if (this.is_player_winner(copy, human)) {
+          return i;
+        }
+      }
+    }
+
+    // Try to take one of the corners, if they are free.
+    let move = this.chooseRandomMoveFromList(board, [0, 3, 7, 8]);
+    if (move !== "") {
+      return move;
+    }
+    // Try to take the center, if it is free.
+    if (this.isSpaceFree(board, 5)) {
+      return 5;
+    }
+    // Move on one of the sides.
+    return this.chooseRandomMoveFromList(board, [2, 4, 6, 8]);
+  }
+
+  chooseRandomMoveFromList(board, list) {
+    let possibleMoves = [];
+
+    for (let i = 0; i < list.length; i++) {
+      if (this.isSpaceFree(board, i)) {
+        possibleMoves.push(i);
+      }
+    }
+
+    if (possibleMoves.length !== 0) {
+      return possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+    } else {
+      return null;
+    }
+  }
+  isSpaceFree(board, move) {
+    return board[move] === " ";
+  }
+
+  makeMove(board, char, move) {
+    board = board.split("");
+    board[move] = char;
   }
 };
--- api/controllers/controllers.js
+++ api/controllers/controllers.js
@@ -20,19 +20,52 @@ exports.game = (req, res) => {
 
   if (ticTac.is_valid(board) === false) {
     res.status(400);
-    res.json({error: "Board contains invalid characters or Player o not allowed to nake a smove"})
+    res.json({
+      error:
+        "Board contains invalid characters or Player o not allowed to nake a smove",
+    });
   }
-  
-  if(ticTac.is_game_tie(board) === true){
+
+  if (ticTac.is_game_tie(board) === true) {
     res.status(400);
-    res.json({message: "It's a tie"})
+    res.json({ message: "It's a tie" });
   }
-  
+
   // Check if player won
-  if(ticTac.is_player_winner(board, 'o')){
-    res.json({message: "Player O won."})
+  if (ticTac.is_player_winner(board, "o")) {
+    res.json({ message: "Player O won." });
   }
-  else{
-    res.json(board)
+
+  // Figuring out the next player
+  var next_player;
+  if (board.split("o").length - 1 - (board.split("x").length - 1) == 1) {
+    next_player = "x";
+  }
+  if (
+    board.split("x").length - 1 - (board.split("o").length - 1) == 1 ||
+    board.split("x").length - 1 - (board.split("o").length - 1) == 0
+  ) {
+    next_player = "o";
+  }
+
+  // If next player is human (o)
+  let curr_move;
+  console.log("Board Before: " + board);
+  if (next_player == "o") {
+    // Get Move
+    curr_move = ticTac.get_move(board, "o");
+    let newBoard = board.split("");
+
+    console.log("Move index " + curr_move);
+    console.log("Board After: " + newBoard);
+
+    const next_board = newBoard.join("");
+    res.json({ next_board: next_board });
+  }
+
+  // if next player is computer (x)
+  if (next_player == "x") {
+    const prospective_board = ticTac.get_prospective_board(board, next_player);
+    res.json({ next_board: prospective_board });
   }
 };
